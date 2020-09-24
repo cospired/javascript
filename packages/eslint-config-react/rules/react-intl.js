586cb06a85db52636e06f2b8e480645e67c8163b
Dirk Haage <dirk@cospired.com>
2020-09-24 15:38:34 +0200

    fix(formatjs): add all rules, even if not documented anymore

--- packages/eslint-config-react/rules/react-intl.js
+++ packages/eslint-config-react/rules/react-intl.js
@@ -73,6 +73,14 @@ module.exports = {
     'formatjs/no-offset': ['error'],
 
     /*
+      Some pipelines enforce automatic ID generation during transpilation using
+      babel-plugin-react-intl or @formatjs/ts-transformer so manual IDs cause issues of
+      mismatching during runtime.
+      https://formatjs.io/docs/tooling/linter#no-id
+    */
+    'formatjs/no-id': ['off'],
+
+    /*
       This enforces generated ID to be set in MessageDescriptor.
       https://formatjs.io/docs/tooling/linter#enforce-id
     */
